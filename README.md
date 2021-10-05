ff29408a4e2bddce37f02da6974b13749fa25e13
naimishparikh <naimish.parikh@gmail.com>
2021-10-05 23:20:47 +0530

    Second Commit

--- README.md
+++ README.md
@@ -1,24 +1,18 @@
 The Casting Agency models a company that is responsible for creating movies and managing and assigning actors to those movies. You are an Executive Producer within the company and are creating a system to simplify and streamline your process.
 
--Installing Python Dependencies
- ------------------------------
+The application is live and hosted at the following URL on heroku
 
-pip install -r requirments.txt
+https://capstoneappnkp10.herokuapp.com/
 
-- Install heroku cli
-
-- Run the python server locally 
-  python app.py
-
-- This will run the server at 127.0.0.1:5000
+You can access the following endpoints
 
 Following are the endpoint apis
 
 Endpoints/API behavior
 GET /actors and /movies - Get all actors and movies
-DELETE /actors/ and /movies/ - Delete the given actor and movie specified with actor id and movie id
+DELETE /actors/actor_id and /movies/movie_id - Delete the given actor and movie specified with actor id and movie id
 POST /actors and /movies and  - Post a new actor or movie
-PATCH /actors/ and /movies/ - Update property of an existing actor or movie
+PATCH /actors/actor_id and /movies/movie_id - Update property of an existing actor or movie
 POST /actorsmovies  - assign actors to movies. And movies to actors
 GET /actorsmovies - get all assigned actors and movies
 
@@ -35,3 +29,55 @@ Executive Producer
     - All permissions a Casting Director has and…
     - Add or delete a movie from the database
     - Assign movies to actors and actors to movies
+
+Following link will provide you access token in the returned url
+
+https://dev-9a27t4db.us.auth0.com/authorize?audience=actormovie&response_type=token&client_id=guRnBpdib3GwPRFPy6qSoNWuLT4nBgu6&redirect_uri=https://127.0.0.1:5000/actors
+
+I have already setup tokens for different roles above with around 24 hrs lifetime for you to test
+
+AUTH0 information for verifying jwt
+
+AUTH0_DOMAIN = 'dev-9a27t4db.us.auth0.com'
+ALGORITHMS = ['RS256']
+API_AUDIENCE = 'actormovie'
+
+Executive Producer token
+-------------------------
+
+eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImkyVkg4X3ZqOERmdHZrVnY4LS1OeCJ9.eyJpc3MiOiJodHRwczovL2Rldi05YTI3dDRkYi51cy5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NjEzYWU3N2E2Mzc2MmMwMDcwYzAwOWM5IiwiYXVkIjoiYWN0b3Jtb3ZpZSIsImlhdCI6MTYzMzQ1Mjc5NCwiZXhwIjoxNjMzNDU5OTk0LCJhenAiOiJndVJuQnBkaWIzR3dQUkZQeTZxU29OV3VMVDRuQmd1NiIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOmFjdG9ycyIsImRlbGV0ZTptb3ZpZXMiLCJnZXQ6YWN0b3JzIiwiZ2V0OmFjdG9yc21vdmllcyIsImdldDptb3ZpZXMiLCJwYXRjaDphY3RvcnMiLCJwYXRjaDptb3ZpZXMiLCJwb3N0OmFjdG9ycyIsInBvc3Q6YWN0b3JzbW92aWVzIiwicG9zdDptb3ZpZXMiXX0.aY4lidE81dkvumf122q1UR1k35aUyzq-rMGM9aA5J4g1rH8hXtZekjzEwN8HZPmdrgBbsFQAT9KLufYbPY90WptI5V8QWVLwmHuiYk65hH6hpZdkKtd5po7wASz2Aq3ih4Pm-mS7xButZpG48Ob5bhAOpqe4Hn7SachVEW3Niki10Hs7xeOvT7-hGiLAUAjxQsq0Sz6-Fr3CZrbd4En1UKASXrlb7rTDMTbfz_ZyEf5Rf5CyCQ5Uaymh7SVuFeAsD6HIa-TYRQeeLvmklDdZAJ2o4C44-Szrw1roJR1p6i4XRvgQQNGNpC9UeIdu5qeQjjSbSm49SzXeMvxuAIEtBQ
+
+Casting Director token
+----------------------
+
+eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImkyVkg4X3ZqOERmdHZrVnY4LS1OeCJ9.eyJpc3MiOiJodHRwczovL2Rldi05YTI3dDRkYi51cy5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NjE0MzQwNjFmNDlmMDYwMDcxNmRmMjMyIiwiYXVkIjoiYWN0b3Jtb3ZpZSIsImlhdCI6MTYzMzQ1MzAyNiwiZXhwIjoxNjMzNDYwMjI2LCJhenAiOiJndVJuQnBkaWIzR3dQUkZQeTZxU29OV3VMVDRuQmd1NiIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOmFjdG9ycyIsImdldDphY3RvcnMiLCJnZXQ6bW92aWVzIiwicGF0Y2g6YWN0b3JzIiwicGF0Y2g6bW92aWVzIiwicG9zdDphY3RvcnMiXX0.cxMDlYkcypv1BN2eSc2ATN3xQsccesfqQrYhYxYlw0RxnIrp4LRn6DhpC1BxZFU8tvlf6GRRX4qnfOPSbNnr1YlM3lTCljQI7ucpI4I5IFbuCDOKBbVOk-5gR7D1wl9ep-j9YVu2wlTLx5PKfQFDAljnMriQ7ALKhss-fyxZ-lJqfwOPOc7MGLTRTKJJHrJWawOKIQY_FpE8bFE-GHm5P5WyFyNjDruJWhc85bw3lXD7X7lqX5NnaQfzeTaN7rXbzUvSZfYdWFaQLbm3Nv_k4pxOh2v7C9oUde4dpZvzYWLUV_VQhCpRkfmmTyCF3KSf1XVy2kaBaOaZvW3oFoNN7w
+
+Casting Assistant token
+-----------------------
+
+eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImkyVkg4X3ZqOERmdHZrVnY4LS1OeCJ9.eyJpc3MiOiJodHRwczovL2Rldi05YTI3dDRkYi51cy5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NjE1OTQyZWQ3MmVmYTgwMDY5YzhmNDdkIiwiYXVkIjoiYWN0b3Jtb3ZpZSIsImlhdCI6MTYzMzQ1MzE2OSwiZXhwIjoxNjMzNDYwMzY5LCJhenAiOiJndVJuQnBkaWIzR3dQUkZQeTZxU29OV3VMVDRuQmd1NiIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiZ2V0OmFjdG9ycyIsImdldDptb3ZpZXMiXX0.BeLiuC-MWSYCBOT0Bjen2qeA93raRaNhLCluc1oaIlkzC-LCeOo2mmMRMEeW1DD52G5aMAAOrLWpkwQJHcRRYidbUK-_rQTuJ55Jk9I0JuM2KFllteqE9vU88RPWKHo0siSQ3PpryI6DYTma54UJEfCCp3HXBcZk3KfsXhUhufg23Lin0u75ibH2S_wXKOhkiqAkzr5Xu87fXbpHXPI6TSCSqCIjxxGggxGmm61825PkBxY1EinebktmH_b0aEzaT63PByaMia6j2xG5Ed-Oi1j1ub65YTowLLE0BS0hrJya2Bdg947TB_zNYrK--FTAP5aFOo_q_GprY5ba6lQprg
+
+
+For testing I have also added a postman collection in following file in the main directory with the successful cases
+
+ActorMovie.postman_collection
+
+The unittest library test suite is available in 
+
+test_app.py
+
+Run as :
+
+python test_app.py
+
+-Installing Python Dependencies
+ ------------------------------
+
+pip install -r requirments.txt
+
+- Install heroku cli
+
+- Run the python server locally 
+  python app.py
+
+- This will run the server at 127.0.0.1:5000
--- test_app.py
+++ test_app.py
@@ -52,9 +52,16 @@ class ActorsMoviesTestCase(unittest.TestCase):
         }
 
 
-        self.headers = {'Content-Type': 'application/json', 'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImkyVkg4X3ZqOERmdHZrVnY4LS1OeCJ9.eyJpc3MiOiJodHRwczovL2Rldi05YTI3dDRkYi51cy5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NjE1OTQyZWQ3MmVmYTgwMDY5YzhmNDdkIiwiYXVkIjoiYWN0b3Jtb3ZpZSIsImlhdCI6MTYzMzQxNDg2NiwiZXhwIjoxNjMzNDIyMDY2LCJhenAiOiJndVJuQnBkaWIzR3dQUkZQeTZxU29OV3VMVDRuQmd1NiIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiZ2V0OmFjdG9ycyIsImdldDptb3ZpZXMiXX0.PALIC4AIliKNkPHdPRPgoPhJwcS5rR55BJ4x0hWliamHd1ENlgUydHRo6UHTFrWhQ-d9E-_z4kLT-TQ4E422n0gJSzldRPZGfhQv5c06V1-FulHhg-m5OGizAQpgBbux3Lc7mj0V4JieYu9I2miMJRB31H5e3QrmhSRSLX13cG0nipQ0Ag89Gmk5r9EzeL4WFxU5Uw56jayxa0wXKIhVmPQreTgAV-gVVa4m3OCk9O92qB3nFrICzsJeOGA9ofn_YPcfYm3kPOj2AwxweVJ0TQpE8EPxqMfO2s67mepOpnTL-hSCzwzZ_f1GNQB2O4B68r9VRYWDEAwqQNtUFdM-7w'}
+        #RBAC based authentication. Following are users with the 3 different roles each
+        # Replace the tokens accordingly
+        self.headersExecutiveProducer = {'Content-Type': 'application/json',
+                         'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImkyVkg4X3ZqOERmdHZrVnY4LS1OeCJ9.eyJpc3MiOiJodHRwczovL2Rldi05YTI3dDRkYi51cy5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NjEzYWU3N2E2Mzc2MmMwMDcwYzAwOWM5IiwiYXVkIjoiYWN0b3Jtb3ZpZSIsImlhdCI6MTYzMzQ1Mjc5NCwiZXhwIjoxNjMzNDU5OTk0LCJhenAiOiJndVJuQnBkaWIzR3dQUkZQeTZxU29OV3VMVDRuQmd1NiIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOmFjdG9ycyIsImRlbGV0ZTptb3ZpZXMiLCJnZXQ6YWN0b3JzIiwiZ2V0OmFjdG9yc21vdmllcyIsImdldDptb3ZpZXMiLCJwYXRjaDphY3RvcnMiLCJwYXRjaDptb3ZpZXMiLCJwb3N0OmFjdG9ycyIsInBvc3Q6YWN0b3JzbW92aWVzIiwicG9zdDptb3ZpZXMiXX0.aY4lidE81dkvumf122q1UR1k35aUyzq-rMGM9aA5J4g1rH8hXtZekjzEwN8HZPmdrgBbsFQAT9KLufYbPY90WptI5V8QWVLwmHuiYk65hH6hpZdkKtd5po7wASz2Aq3ih4Pm-mS7xButZpG48Ob5bhAOpqe4Hn7SachVEW3Niki10Hs7xeOvT7-hGiLAUAjxQsq0Sz6-Fr3CZrbd4En1UKASXrlb7rTDMTbfz_ZyEf5Rf5CyCQ5Uaymh7SVuFeAsD6HIa-TYRQeeLvmklDdZAJ2o4C44-Szrw1roJR1p6i4XRvgQQNGNpC9UeIdu5qeQjjSbSm49SzXeMvxuAIEtBQ'}
 
+        self.headersCastingDirector = {'Content-Type': 'application/json',
+                         'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImkyVkg4X3ZqOERmdHZrVnY4LS1OeCJ9.eyJpc3MiOiJodHRwczovL2Rldi05YTI3dDRkYi51cy5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NjE0MzQwNjFmNDlmMDYwMDcxNmRmMjMyIiwiYXVkIjoiYWN0b3Jtb3ZpZSIsImlhdCI6MTYzMzQ1MzAyNiwiZXhwIjoxNjMzNDYwMjI2LCJhenAiOiJndVJuQnBkaWIzR3dQUkZQeTZxU29OV3VMVDRuQmd1NiIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiZGVsZXRlOmFjdG9ycyIsImdldDphY3RvcnMiLCJnZXQ6bW92aWVzIiwicGF0Y2g6YWN0b3JzIiwicGF0Y2g6bW92aWVzIiwicG9zdDphY3RvcnMiXX0.cxMDlYkcypv1BN2eSc2ATN3xQsccesfqQrYhYxYlw0RxnIrp4LRn6DhpC1BxZFU8tvlf6GRRX4qnfOPSbNnr1YlM3lTCljQI7ucpI4I5IFbuCDOKBbVOk-5gR7D1wl9ep-j9YVu2wlTLx5PKfQFDAljnMriQ7ALKhss-fyxZ-lJqfwOPOc7MGLTRTKJJHrJWawOKIQY_FpE8bFE-GHm5P5WyFyNjDruJWhc85bw3lXD7X7lqX5NnaQfzeTaN7rXbzUvSZfYdWFaQLbm3Nv_k4pxOh2v7C9oUde4dpZvzYWLUV_VQhCpRkfmmTyCF3KSf1XVy2kaBaOaZvW3oFoNN7w'}
 
+        self.headersCastingAssistant = {'Content-Type': 'application/json',
+                                       'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImkyVkg4X3ZqOERmdHZrVnY4LS1OeCJ9.eyJpc3MiOiJodHRwczovL2Rldi05YTI3dDRkYi51cy5hdXRoMC5jb20vIiwic3ViIjoiYXV0aDB8NjE1OTQyZWQ3MmVmYTgwMDY5YzhmNDdkIiwiYXVkIjoiYWN0b3Jtb3ZpZSIsImlhdCI6MTYzMzQ1MzE2OSwiZXhwIjoxNjMzNDYwMzY5LCJhenAiOiJndVJuQnBkaWIzR3dQUkZQeTZxU29OV3VMVDRuQmd1NiIsInNjb3BlIjoiIiwicGVybWlzc2lvbnMiOlsiZ2V0OmFjdG9ycyIsImdldDptb3ZpZXMiXX0.BeLiuC-MWSYCBOT0Bjen2qeA93raRaNhLCluc1oaIlkzC-LCeOo2mmMRMEeW1DD52G5aMAAOrLWpkwQJHcRRYidbUK-_rQTuJ55Jk9I0JuM2KFllteqE9vU88RPWKHo0siSQ3PpryI6DYTma54UJEfCCp3HXBcZk3KfsXhUhufg23Lin0u75ibH2S_wXKOhkiqAkzr5Xu87fXbpHXPI6TSCSqCIjxxGggxGmm61825PkBxY1EinebktmH_b0aEzaT63PByaMia6j2xG5Ed-Oi1j1ub65YTowLLE0BS0hrJya2Bdg947TB_zNYrK--FTAP5aFOo_q_GprY5ba6lQprg'}
         # binds the app to the current context
         with self.app.app_context():
             self.db = SQLAlchemy()
@@ -71,8 +78,10 @@ class ActorsMoviesTestCase(unittest.TestCase):
     Write at least one test for each test for successful operation and for expected errors.
     """
 
+    #Test cases for executive producer with all permissions
+
     def test_get_actors(self):
-        res = self.client().get('/actors',headers=self.headers)
+        res = self.client().get('/actors',headers=self.headersExecutiveProducer)
         print("res.data",res.data)
         print("res.data type", type(res.data))
         #binary bytes to string. using ascii decode
@@ -91,7 +100,7 @@ class ActorsMoviesTestCase(unittest.TestCase):
 
     def test_get_actors_failue(self):
         #failure /movie instead of movies
-        res = self.client().get('/actor',headers=self.headers)
+        res = self.client().get('/actor',headers=self.headersExecutiveProducer)
         print("res.data",res.data)
         print("res.data type", type(res.data))
         str = b'I am a string'.decode('ASCII')
@@ -106,7 +115,7 @@ class ActorsMoviesTestCase(unittest.TestCase):
 
 
     def test_get_movies(self):
-        res = self.client().get('/movies',headers=self.headers)
+        res = self.client().get('/movies',headers=self.headersExecutiveProducer)
         print("res.data",res.data)
         print("res.data type", type(res.data))
         str = b'I am a string'.decode('ASCII')
@@ -120,7 +129,7 @@ class ActorsMoviesTestCase(unittest.TestCase):
 
     def test_get_movies_failue(self):
         #failure /movie instead of movies
-        res = self.client().get('/movie',headers=self.headers)
+        res = self.client().get('/movie',headers=self.headersExecutiveProducer)
         print("res.data",res.data)
         print("res.data type", type(res.data))
         str = b'I am a string'.decode('ASCII')
@@ -134,7 +143,7 @@ class ActorsMoviesTestCase(unittest.TestCase):
 
     def test_post_movie(self):
 
-        res = self.client().post('/movies', json=self.new_movie, headers=self.headers)
+        res = self.client().post('/movies', json=self.new_movie, headers=self.headersExecutiveProducer)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -142,7 +151,7 @@ class ActorsMoviesTestCase(unittest.TestCase):
         self.assertTrue(len(data['movies']))
 
     def test_post_movie_failure(self):
-        res = self.client().post('/movie', json=self.new_movie, headers=self.headers)
+        res = self.client().post('/movie', json=self.new_movie, headers=self.headersExecutiveProducer)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
@@ -150,7 +159,7 @@ class ActorsMoviesTestCase(unittest.TestCase):
 
 
     def test_post_actor(self):
-        res = self.client().post('/actors', json=self.new_actor, headers=self.headers)
+        res = self.client().post('/actors', json=self.new_actor, headers=self.headersExecutiveProducer)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -158,14 +167,14 @@ class ActorsMoviesTestCase(unittest.TestCase):
         self.assertTrue(len(data['actors']))
 
     def test_post_actor_failure(self):
-        res = self.client().post('/actor', json=self.new_actor, headers=self.headers)
+        res = self.client().post('/actor', json=self.new_actor, headers=self.headersExecutiveProducer)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
 
     def test_delete_actor(self):
-        res = self.client().delete('/actors/12', headers=self.headers)
+        res = self.client().delete('/actors/12', headers=self.headersExecutiveProducer)
         print("after delete actor")
         data = json.loads(res.data)
 
@@ -174,14 +183,14 @@ class ActorsMoviesTestCase(unittest.TestCase):
         self.assertTrue(data['delete'])
 
     def test_delete_actor_failure(self):
-        res = self.client().delete('/movie', headers=self.headers)
+        res = self.client().delete('/movie', headers=self.headersExecutiveProducer)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
 
     def test_delete_movie(self):
-        res = self.client().delete('/movies/12', headers=self.headers)
+        res = self.client().delete('/movies/12', headers=self.headersExecutiveProducer)
         print("after delete actor")
         data = json.loads(res.data)
 
@@ -190,7 +199,7 @@ class ActorsMoviesTestCase(unittest.TestCase):
         self.assertTrue(data['delete'])
 
     def test_delete_actor_failure(self):
-        res = self.client().delete('/movie', headers=self.headers)
+        res = self.client().delete('/movie', headers=self.headersExecutiveProducer)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
@@ -199,7 +208,7 @@ class ActorsMoviesTestCase(unittest.TestCase):
 
 
     def test_patch_actor(self):
-        res = self.client().patch('/actors/2',json=self.modify_actor_forpatch, headers=self.headers)
+        res = self.client().patch('/actors/2',json=self.modify_actor_forpatch, headers=self.headersExecutiveProducer)
         print("after delete actor")
         data = json.loads(res.data)
 
@@ -208,14 +217,14 @@ class ActorsMoviesTestCase(unittest.TestCase):
         self.assertTrue(len(data['actors']))
 
     def test_patch_actor_failure(self):
-        res = self.client().patch('/actor', headers=self.headers)
+        res = self.client().patch('/actor', headers=self.headersExecutiveProducer)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
 
     def test_patch_movie(self):
-        res = self.client().patch('/movies/2',json=self.modify_movie_forpatch, headers=self.headers)
+        res = self.client().patch('/movies/2',json=self.modify_movie_forpatch, headers=self.headersExecutiveProducer)
         print("after delete actor")
         data = json.loads(res.data)
 
@@ -224,14 +233,14 @@ class ActorsMoviesTestCase(unittest.TestCase):
         self.assertTrue(len(data['movies']))
 
     def test_patch_movie_failure(self):
-        res = self.client().patch('/movie', headers=self.headers)
+        res = self.client().patch('/movie', headers=self.headersExecutiveProducer)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
 
     def test_post_actormovie(self):
-        res = self.client().post('/actorsmovies',json=self.actorsmovies, headers=self.headers)
+        res = self.client().post('/actorsmovies',json=self.actorsmovies, headers=self.headersExecutiveProducer)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 200)
@@ -239,14 +248,14 @@ class ActorsMoviesTestCase(unittest.TestCase):
         self.assertTrue(len(data['actorsmovies']))
 
     def test_post_actormovie_failure(self):
-        res = self.client().post('/actorsmovie',json=self.actorsmovies, headers=self.headers)
+        res = self.client().post('/actorsmovie',json=self.actorsmovies, headers=self.headersExecutiveProducer)
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
 
     def test_get_actormovie(self):
-        res = self.client().get('/actorsmovies',headers=self.headers)
+        res = self.client().get('/actorsmovies',headers=self.headersExecutiveProducer)
 
         data = json.loads(res.data)
 
@@ -255,12 +264,60 @@ class ActorsMoviesTestCase(unittest.TestCase):
         self.assertTrue(len(data['actorsmovies']))
 
     def test_get_actormovie_failure(self):
-        res = self.client().get('/actorsmovie', headers=self.headers)
+        res = self.client().get('/actorsmovie', headers=self.headersExecutiveProducer)
         print("after delete actor")
         data = json.loads(res.data)
 
         self.assertEqual(res.status_code, 404)
         self.assertEqual(data['success'], False)
+
+
+#test cases for Casting Director
+    def test_get_actors_cd(self):
+        res = self.client().get('/actors',headers=self.headersCastingDirector)
+        print("res.data",res.data)
+        print("res.data type", type(res.data))
+
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['actors']))
+
+    #Casting Director does not have access to get actorsmovies
+    def test_get_actormovie_failure_cd(self):
+        res = self.client().get('/actorsmovies', headers=self.headersCastingDirector)
+        print("after delete actor")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 403)
+        self.assertEqual(data['success'], False)
+
+
+
+    #test cases for Casting Assistant
+    def test_get_actors_ca(self):
+        res = self.client().get('/actors', headers=self.headersCastingAssistant)
+        print("res.data", res.data)
+        print("res.data type", type(res.data))
+
+        data = json.loads(res.data)
+
+        print("DATA in test get actors",data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['actors']))
+
+    # Casting Assistant does not have access to get actorsmovies
+    def test_get_actormovie_failure_ca(self):
+        res = self.client().get('/actorsmovies', headers=self.headersCastingAssistant)
+        print("after delete actor")
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 403)
+        self.assertEqual(data['success'], False)
+
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
